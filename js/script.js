23d36cfab39ba3a4820d8b3ab0d4b81d008e717b
FilipeHibrael <filipehibrael27@gmail.com>
2023-08-07 16:29:44 -0300

    commit inicial

---
+++ js/script.js
@@ -0,0 +1,23 @@
+import initScollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/srcoll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+import initmodal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
+import initFuncionamento from './modules/funcionamento.js';
+import initFetchAnimais from './modules/fetch-animais.js';
+import initFetchBitcoin from './modules/fetch-bitcoin.js';
+
+initScollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initmodal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initFuncionamento();
+initFetchAnimais();
+initFetchBitcoin();
