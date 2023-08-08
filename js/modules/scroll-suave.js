bab4015971979182d9355c2833dee9f61fd56ae5
FilipeHibrael <filipehibrael27@gmail.com>
2023-08-07 23:40:28 -0300

    ScrollSuave

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,25 +1,33 @@
-export default function initScollSuave() {
-  const linkInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.linkInternos = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = { behavior: 'smooth', block: 'start' };
+    } else {
+      this.options = options;
+    }
 
-  function scrollToSection(event) {
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
+
+  scrollToSection(event) {
     event.preventDefault();
     const href = event.currentTarget.getAttribute('href');
     const section = document.querySelector(href);
 
-    section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
-
-    // Forma Alternativa
-    // const topo = section.offsetTop
-    // window.scrollTo({
-    //     top: topo,
-    //     behavior: 'smooth',
-    // })
+    section.scrollIntoView(this.options);
   }
 
-  linkInternos.forEach((link) => {
-    link.addEventListener('click', scrollToSection);
-  });
+  addLinkEvent() {
+    this.linkInternos.forEach((link) => {
+      link.addEventListener('click', this.scrollToSection);
+    });
+  }
+
+  init() {
+    if (this.linkInternos.length) {
+      this.addLinkEvent();
+    }
+    return this;
+  }
 }
